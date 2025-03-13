ebd9da36c21f3bbe41e9b7adc5d0a23d9d8c95e0
Paul Walker <paul@pwjw.com>
2025-03-12 20:22:38 -0400

    Enough to see that proces sis being called properly etc

--- src/instances/note-generator.h
+++ src/instances/note-generator.h
@@ -23,6 +23,42 @@ struct NoteGeneratorPlugin : CVPClap<ValidatorFlavor::NoteGenerator>
         : CVPClap<ValidatorFlavor::NoteGenerator>(host)
     {
     }
+
+  protected:
+    bool startProcessing() noexcept override
+    {
+      return true;
+    }
+    void stopProcessing() noexcept override
+    {
+
+    }
+
+    double priorBeat{-1234576960};
+    bool isNoteOn{false};
+    clap_process_status process(const clap_process *process) noexcept override
+    {
+      if (process->transport)
+      {
+        auto bt = 1.0 * process->transport->song_pos_beats / CLAP_BEATTIME_FACTOR;
+        if (std::floor(bt) != std::floor(priorBeat))
+        {
+          priorBeat = bt;
+          log(CLAP_LOG_INFO, "Got another beat");
+          isNoteOn = true;
+        }
+        if (bt - priorBeat > 0.5 && isNoteOn)
+        {
+          log(CLAP_LOG_INFO, "Note Off");
+          isNoteOn = false;
+        }
+      }
+      return CLAP_PROCESS_CONTINUE;
+    }
+    void reset() noexcept override
+    {
+
+    }
 };
 } // namespace free_audio::cvp
 
