383e94299c7f08805f532f84ddc738622d5e67c3
Paul <baconpaul@users.noreply.github.com>
2025-03-13 10:54:29 -0400

    A bit more gui pre-plumbing (#2)
    * A bit more gui pre-plumbing
    
    Really just a checkpoint though to test the PR workflow from
    a fork. But we have a class for macOS which gets factorieid on
    gui create.

--- src/gui/gui-macos.mm
+++ src/gui/gui-macos.mm
@@ -1,16 +1,34 @@
-//
-// Created by Paul Walker on 3/13/25.
-//
+/*
+ * Clap Validator Plugin
+ *
+ * A expanding set of plugins to test your host
+ *
+ * Copyright 2024-2025,Various authors, as described in the github
+ * transaction log.
+ *
+ * This source repo is released under the MIT license,
+ * The source code and license are at https://github.com/free-audio/clap-validator-plugin
+ */
 
 #include "gui.h"
 #include <clap/clap.h>
 
 namespace free_audio::cvp::gui
 {
-
-std::unique_ptr<GuiProvider> createGuiProvider(std::function<void(clap_log_severity, const std::string &)> logFn)
+namespace detail
 {
-    logFn(CLAP_LOG_WARNING, "MacOS GUI Unimplemented");
-    return nullptr;
+struct MacOSGUIProvider : GuiProvider
+{
+    MacOSGUIProvider(const logFn_t &logFn) : GuiProvider(logFn)
+    {
+        logFmt(CLAP_LOG_INFO, "MacOS GUI Created (unimplemented)");
+    }
+};
+} // namespace detail
+
+std::unique_ptr<GuiProvider>
+createGuiProvider(std::function<void(clap_log_severity, const std::string &)> logFn)
+{
+    return std::make_unique<detail::MacOSGUIProvider>(logFn);
 }
-}
+} // namespace free_audio::cvp::gui
